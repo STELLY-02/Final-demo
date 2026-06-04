ff3df7a87ae2f3bf4929773965962bb6417a5a88
STELLY-02 <116903730+STELLY-02@users.noreply.github.com>
2026-06-04 14:30:10 +0800

    Change pronoun in documentation paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, she answered</p>
+          <p>Your questions, he answered</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
